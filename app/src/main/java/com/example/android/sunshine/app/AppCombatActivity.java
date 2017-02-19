6f13b21403fbf238b4091b306a6c70e1418bdc87
Nadya Prabaningrum <nadya.prabaningrum@mail.ugm.ac.id>
2017-02-19 20:01:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android/sunshine/app/AppCombatActivity.java
@@ -0,0 +1,11 @@
+package com.example.android.sunshine.app;
+
+import android.os.Bundle;
+
+/**
+ * Created by nad on 13/11/2016.
+ */
+public class AppCombatActivity {
+    public void onCreate(Bundle savedInstanceState) {
+    }
+}
